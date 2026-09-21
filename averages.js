99714cbb5f08bf16164a0ae818d9b40299032d1c
Your Name <you@example.com>
2026-09-21 09:58:41 -0300

    Time storing and average calculation

--- averages.js
+++ averages.js
@@ -2,12 +2,16 @@ const storedTimesArr = [];
 
 function storeTimeForAvg(storedTime) {
     storedTime = (storedTime / 1000).toFixed(2);
-    storedTimesArr.push(storedTime);
+    storedTimesArr.push(parseFloat(storedTime));
+    const average = storedTimesArr.reduce((sum, n) => sum + n, 0) / storedTimesArr.length;
     const avgRightSidebar = document.getElementById("avgRightSidebar")
     if(avgRightSidebar.textContent === "-") {
         avgRightSidebar.textContent = "1 - " + storedTime;
     } else {
-        avgRightSidebar.innerHTML += "<br>" + storedTimesArr.length + " - " + storedTime;
+        avgRightSidebar.innerHTML = "Average: " + average.toFixed(2);
+        storedTimesArr.forEach((time, index) => {
+            avgRightSidebar.innerHTML += "</br>" + (index + 1) + " - " + time;
+        });
     }
 } 
 
